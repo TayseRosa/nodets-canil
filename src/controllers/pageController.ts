5f69c3157cb0a51c2874dbfcd43a87eb2d7f4d59
Tayse <tayse_rosa@yahoo.com.br>
2022-04-22 14:36:56 -0300

    Base criada

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res:Response) => {
+  /* res.render('pages/page') */
+}
+
+export const dogs = (req: Request, res:Response) => {
+  /* res.render('pages/page') */
+}
+
+export const cats = (req: Request, res:Response) => {
+  /* res.render('pages/page') */
+}
+
+export const fishes = (req: Request, res:Response) => {
+  /* res.render('pages/page') */
+}
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+import path from 'path'
+import mainRoutes from './routes/index'
+
+dotenv.config();
+
+const server = express();
+
+/* Config Template Engine */
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+/* Config Pasta pública */
+server.use(express.static(path.join(__dirname, '../public')));
+
+/* Rotas */
+server.use(mainRoutes)
+
+/* Página de não encontrado */
+server.use((req, res)=>{
+  res.send('Página não encontrada');
+})
+
+/* Servidor rodando.. */
+server.listen(process.env.PORT);
