0a944cea0131eb9aaa846a60d3e658e934d740e8
Tayse <tayse_rosa@yahoo.com.br>
2022-04-22 16:15:15 -0300

    Finnaly project

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -6,6 +6,11 @@ import { createMenuObject } from '../helpers/createMenuObject'
 export const search = (req: Request, res:Response) => {
   let query :string = req.query.q as string;
 
+  if(!query){
+    res.redirect('/');
+    return;
+  }
+
   let list = Pet.getFromName(query);
 
   res.render('pages/page', {
