267a4028cef2b4a756b6dc4504ef96a6a3c3d2bd
Tayse <tayse_rosa@yahoo.com.br>
2022-04-22 16:06:08 -0300

    Fazendo a busca(search)

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from "express";
 
+import { Pet } from "../models/pet";
+import { createMenuObject } from '../helpers/createMenuObject'
+
 export const search = (req: Request, res:Response) => {
-  /* res.render('pages/search') */
+  let query :string = req.query.q as string;
+
+  let list = Pet.getFromName(query);
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list
+  })
 }
